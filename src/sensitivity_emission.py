59f8100561200c5ff4290519c5f3af07a61605bc
hchqin <143662050+hchqin@users.noreply.github.com>
2024-05-28 20:58:57 -0700

    Add Sci ef calc and sci emission calc class (#74)
    * Add new class for computing ef for sensitivity analysis
    
    * Add class for emission sensitivity analysis
    
    * Add operation_mode argu to scienti ef and emission calc class

---
+++ src/sensitivity_emission.py
@@ -0,0 +1,69 @@
+from src.emission_calculator import EmissionCalculator
+import numpy as np
+class SensitivityEmission:
+    def __init__(self, ef_data, n_data, operation_mode = 'farmer'):
+        self.ef_data = ef_data
+        self.n_data = n_data
+        self.variables = list(ef_data.keys()) + list(n_data.keys())
+        self.mode = operation_mode
+        self.results = {}
+        self.output = {}
+
+    def perform_analysis(self):
+        # Iterating over all variables
+        for variable in self.variables:
+            # Initialize dictionary for each variable to store lists of results
+            self.results[variable] = {}
+            # Extract array values for the current variable
+            if variable in self.ef_data.keys():
+                values_array = self.ef_data.get(variable).get('EF')
+            elif variable in self.n_data.keys():
+                values_array = self.n_data.get(variable).get('n_crop_residue')
+            
+            # Initialize results storage for this variable
+            init_dict = {}
+            for index in range(len(values_array)):
+                modified_ef = self.prepare_ef_input_for_ec(variable, values_array[index])
+                modified_n = self.prepare_n_input_for_ec(variable, values_array[index])
+                ec = EmissionCalculator(modified_ef, modified_n)
+                emission_results = ec.get_emission()
+                
+                # Initialize dictionary to collect results
+                if index == 0:  # Setup the dictionary with keys and empty lists
+                    for key in emission_results.keys():
+                        init_dict[key] = []
+                
+                # Append results to the corresponding key
+                for key, value in emission_results.items():
+                    init_dict[key].append(value)
+            
+            # Store aggregated results
+            for key, value_list in init_dict.items():
+                self.results[variable][key] = np.array(value_list)
+        
+        return self.results
+    
+    def get_result(self):
+        output_temp = self.perform_analysis()
+        if self.mode == 'farmer':
+            self.output = output_temp['P']
+        elif self.mode == 'scientific':
+            self.output = output_temp
+            
+        return self.output
+    
+    def prepare_ef_input_for_ec(self, selected_variable, value):
+
+        ef_input = {'EF': self.ef_data.get('P').get('EF')[0]}
+        if selected_variable in self.ef_data:
+            ef_input['EF'] = value
+        return ef_input
+
+
+    def prepare_n_input_for_ec(self, selected_variable, value):
+        n_input = {'n_crop_residue': self.n_data.get('moisture').get('n_crop_residue')[0]}
+        
+        if selected_variable in self.n_data:
+            n_input['n_crop_residue'] = value
+        return n_input  
+    
---
+++ src/sensitivity_emission_factor.py
@@ -0,0 +1,65 @@
+import numpy as np
+from src.emission_factor_calculator import EmissionFactorCalculator
+class SensitivityEmissionFactor:
+    def __init__(self, farm_data, operation_mode = 'farmer'):
+        self.farm_data = farm_data
+        self.variables = list(farm_data['climate_data'].keys()) + list(farm_data['modifiers'].keys())
+        self.mode = operation_mode
+        self.results = {}
+        self.output = {}
+
+    def perform_analysis(self):
+        # Iterating over all variables
+        for variable in self.variables:
+            # Initialize dictionary for each variable to store lists of results
+            self.results[variable] = {}
+            # Extract array values for the current variable
+            values_array = self.farm_data['climate_data'].get(variable, []) + self.farm_data['modifiers'].get(variable, [])
+            
+            # Initialize results storage for this variable
+            init_dict = {}
+            for index in range(len(values_array)):
+                modified_data = self.prepare_data_for_efc(variable, values_array[index])
+                efc = EmissionFactorCalculator(modified_data)
+                ef_results = efc.get_ef()
+                
+                # Initialize dictionary to collect results by ef type
+                if index == 0:  # Setup the dictionary with keys and empty lists
+                    for key in ef_results.keys():
+                        init_dict[key] = []
+                
+                # Append results to the corresponding key
+                for key, value in ef_results.items():
+                    init_dict[key].append(value)
+            
+            # Store aggregated results
+            for key, value_list in init_dict.items():
+                self.results[variable][key] = np.array(value_list)
+        
+        return self.results
+    
+    def get_result(self):
+        output_temp = self.perform_analysis()
+        if self.mode == 'farmer':
+            self.output = output_temp['P']
+        elif self.mode == 'scientific':
+            self.output = output_temp
+            
+        return self.output
+
+    def prepare_data_for_efc(self, selected_variable, value):
+        data = {'climate_data': {}, 'modifiers': {}}
+        # Initialize all variables with their default values
+        for var in self.variables:
+            if var in self.farm_data['climate_data']:
+                data['climate_data'][var] = self.farm_data['climate_data'][var][0]
+            if var in self.farm_data['modifiers']:
+                data['modifiers'][var] = self.farm_data['modifiers'][var][0]
+        
+        # Replace the value of the selected variable with the current value from its array
+        if selected_variable in data['climate_data']:
+            data['climate_data'][selected_variable] = value
+        elif selected_variable in data['modifiers']:
+            data['modifiers'][selected_variable] = value
+        
+        return data
